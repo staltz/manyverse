cfb71774e93d777d3a3c5f4330c1de4798b09905
Andre Staltz <andre@staltz.com>
2018-02-16 12:42:39 +0200

    dx: add rn-cli.config to customize bundler blacklist

---
+++ rn-cli.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const sharedBlacklist = [
+  /node_modules[/\\]react[/\\]dist[/\\].*/,
+
+  // Background process should not be known by the react-native bundler
+  /rnnodeapp\/.*/,
+  /src\/rnnodeapp\/.*/,
+];
+
+const platformBlacklists = {
+  web: ['.ios.js', '.android.js'],
+  ios: ['.web.js', '.android.js'],
+  android: ['.web.js', '.ios.js'],
+};
+
+function escapeRegExp(pattern) {
+  if (Object.prototype.toString.call(pattern) === '[object RegExp]') {
+    return pattern.source.replace(/\//g, path.sep);
+  } else if (typeof pattern === 'string') {
+    const escaped = pattern.replace(
+      /[\-\[\]\{\}\(\)\*\+\?\.\\\^\$\|]/g,
+      '\\$&',
+    );
+    // convert the '/' into an escaped local file separator
+    return escaped.replace(/\//g, `\\${path.sep}`);
+  }
+  throw new Error(`Unexpected packager blacklist pattern: ${pattern}`);
+}
+
+function blacklist(platform, additionalBlacklist) {
+  return new RegExp(
+    '(' +
+      (additionalBlacklist || [])
+        .concat(sharedBlacklist)
+        .concat(platformBlacklists[platform] || [])
+        .map(escapeRegExp)
+        .join('|') +
+      ')$',
+  );
+}
+
+module.exports = {
+  getBlacklistRE(platform) {
+    return blacklist(platform);
+  },
+};
