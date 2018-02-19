c784e0d57327bc9a1a940f7a17f470402230601e
Andre Staltz <andre@staltz.com>
2018-02-19 19:19:39 +0200

    dx: convert rnnodeapp from JS to TypeScript

--- src/rnnodeapp/index.ts
+++ src/rnnodeapp/index.ts
@@ -1,9 +1,29 @@
+/**
+ * MMMMM is a mobile app for Secure Scuttlebutt networks
+ *
+ * Copyright (C) 2017 Andre 'Staltz' Medeiros
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const ssbKeys = require('ssb-keys');
 const mkdirp = require('mkdirp');
-const manifest = require('./manifest');
+import manifest = require('./manifest');
+import rootsPlugin = require('./roots');
 
 const ssbPath = path.resolve(os.homedir(), '.ssb');
 if (!fs.existsSync(ssbPath)) {
@@ -29,7 +49,7 @@ require('scuttlebot/index')
   .use(require('ssb-about'))
   .use(require('ssb-contacts'))
   .use(require('ssb-query'))
-  .use(require('./roots'))
+  .use(rootsPlugin)
   .use(require('scuttlebot/plugins/invite'))
   .use(require('scuttlebot/plugins/block'))
   .use(require('scuttlebot/plugins/local'))
