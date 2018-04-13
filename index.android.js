69aa6bacbc78cfe4662c0dbe9fcec93bf7f6c5ab
luandro <luandro@gmail.com>
2018-04-13 18:45:27 -0300

    Trying to install nodejs-mobile

--- index.android.js
+++ index.android.js
@@ -1,63 +1,68 @@
-/**
- * MMMMM is a mobile app for Secure Scuttlebutt networks
- *
- * Copyright (C) 2017 Andre 'Staltz' Medeiros
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// /**
+//  * MMMMM is a mobile app for Secure Scuttlebutt networks
+//  *
+//  * Copyright (C) 2017 Andre 'Staltz' Medeiros
+//  *
+//  * This program is free software: you can redistribute it and/or modify
+//  * it under the terms of the GNU General Public License as published by
+//  * the Free Software Foundation, either version 3 of the License, or
+//  * (at your option) any later version.
+//  *
+//  * This program is distributed in the hope that it will be useful,
+//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  * GNU General Public License for more details.
+//  *
+//  * You should have received a copy of the GNU General Public License
+//  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  */
 
-import 'react-native-ssb-shims';
-import {run} from '@cycle/run';
-import {makeScreenDriver} from '@cycle/native-screen';
-import {app} from './lib/app/index';
-import onionify from 'cycle-onionify';
-import {ssbDriver} from './lib/app/drivers/ssb';
-import {dialogDriver} from './lib/app/drivers/dialogs';
-import {makeSingleScreenNavDrivers} from 'cycle-native-navigation';
-import RNNav from 'react-native-navigation';
-import {Palette} from './lib/app/global-styles/palette';
-import {Dimensions} from './lib/app/global-styles/dimens';
-import {Typography} from './lib/app/global-styles/typography';
-import {navigatorStyle as centralNavigatorStyle} from './lib/app/screens/central/styles';
-import RNNode from 'react-native-node';
+// import 'react-native-ssb-shims';
+// import {run} from '@cycle/run';
+// import {makeScreenDriver} from '@cycle/native-screen';
+// import {app} from './lib/app/index';
+// import onionify from 'cycle-onionify';
+// import {ssbDriver} from './lib/app/drivers/ssb';
+// import {dialogDriver} from './lib/app/drivers/dialogs';
+// import {makeSingleScreenNavDrivers} from 'cycle-native-navigation';
+// import RNNav from 'react-native-navigation';
+// import {Palette} from './lib/app/global-styles/palette';
+// import {Dimensions} from './lib/app/global-styles/dimens';
+// import {Typography} from './lib/app/global-styles/typography';
+// import {navigatorStyle as centralNavigatorStyle} from './lib/app/screens/central/styles';
+// import nodejs from 'nodejs-mobile-react-native';
 
-const {screenVNodeDriver, commandDriver} = makeSingleScreenNavDrivers(
-  RNNav,
-  ['mmmmm.Central', 'mmmmm.Profile', 'mmmmm.Profile.Edit'],
-  {
-    screen: {
-      screen: 'mmmmm.Central',
-      navigatorStyle: centralNavigatorStyle,
-    },
-    animationType: 'fade',
-  },
-);
+// const {screenVNodeDriver, commandDriver} = makeSingleScreenNavDrivers(
+//   RNNav,
+//   ['mmmmm.Central', 'mmmmm.Profile', 'mmmmm.Profile.Edit'],
+//   {
+//     screen: {
+//       screen: 'mmmmm.Central',
+//       navigatorStyle: centralNavigatorStyle,
+//     },
+//     animationType: 'fade',
+//   },
+// );
 
-RNNode.start();
+// nodejs.start("main.js");
 
-RNNav.Navigation.isAppLaunched().then(appLaunched => {
-  if (appLaunched) {
-    startCycleApp();
-  }
-  new RNNav.NativeEventsReceiver().appLaunched(startCycleApp);
-});
+// RNNav.Navigation.isAppLaunched().then(appLaunched => {
+//   if (appLaunched) {
+//     startCycleApp();
+//   }
+//   new RNNav.NativeEventsReceiver().appLaunched(startCycleApp);
+// });
 
-function startCycleApp() {
-  run(onionify(app), {
-    screen: screenVNodeDriver,
-    navigation: commandDriver,
-    ssb: ssbDriver,
-    dialog: dialogDriver,
-  });
-}
+// function startCycleApp() {
+//   run(onionify(app), {
+//     screen: screenVNodeDriver,
+//     navigation: commandDriver,
+//     ssb: ssbDriver,
+//     dialog: dialogDriver,
+//   });
+// }
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('ScuttlebotMobile', () => App);
--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,44 +1,56 @@
-const path = require('path');
+// const path = require('path');
 
-const sharedBlacklist = [
-  /node_modules[/\\]react[/\\]dist[/\\].*/,
-  /src\/rnnodeapp\/.*/,
-];
+// const sharedBlacklist = [
+//   /node_modules[/\\]react[/\\]dist[/\\].*/,
+//   /src\/rnnodeapp\/.*/,
+// ];
 
-const platformBlacklists = {
-  web: ['.ios.js', '.android.js'],
-  ios: ['.web.js', '.android.js'],
-  android: ['.web.js', '.ios.js'],
-};
+// const platformBlacklists = {
+//   web: ['.ios.js', '.android.js'],
+//   ios: ['.web.js', '.android.js'],
+//   android: ['.web.js', '.ios.js'],
+// };
 
-function escapeRegExp(pattern) {
-  if (Object.prototype.toString.call(pattern) === '[object RegExp]') {
-    return pattern.source.replace(/\//g, path.sep);
-  } else if (typeof pattern === 'string') {
-    const escaped = pattern.replace(
-      /[\-\[\]\{\}\(\)\*\+\?\.\\\^\$\|]/g,
-      '\\$&',
-    );
-    // convert the '/' into an escaped local file separator
-    return escaped.replace(/\//g, `\\${path.sep}`);
-  }
-  throw new Error(`Unexpected packager blacklist pattern: ${pattern}`);
-}
+// function escapeRegExp(pattern) {
+//   if (Object.prototype.toString.call(pattern) === '[object RegExp]') {
+//     return pattern.source.replace(/\//g, path.sep);
+//   } else if (typeof pattern === 'string') {
+//     const escaped = pattern.replace(
+//       /[\-\[\]\{\}\(\)\*\+\?\.\\\^\$\|]/g,
+//       '\\$&',
+//     );
+//     // convert the '/' into an escaped local file separator
+//     return escaped.replace(/\//g, `\\${path.sep}`);
+//   }
+//   throw new Error(`Unexpected packager blacklist pattern: ${pattern}`);
+// }
 
-function blacklist(platform, additionalBlacklist) {
-  return new RegExp(
-    '(' +
-      (additionalBlacklist || [])
-        .concat(sharedBlacklist)
-        .concat(platformBlacklists[platform] || [])
-        .map(escapeRegExp)
-        .join('|') +
-      ')$',
-  );
-}
+// function blacklist(platform, additionalBlacklist) {
+//   return new RegExp(
+//     '(' +
+//       (additionalBlacklist || [])
+//         .concat(sharedBlacklist)
+//         .concat(platformBlacklists[platform] || [])
+//         .map(escapeRegExp)
+//         .join('|') +
+//       ')$',
+//   );
+// }
+
+// module.exports = {
+//   getBlacklistRE(platform) {
+//     return blacklist(platform);
+//   },
+// };
+
+const blacklist = require('metro/src/blacklist');
 
 module.exports = {
-  getBlacklistRE(platform) {
-    return blacklist(platform);
+  getBlacklistRE: function() {
+    return blacklist([
+        /nodejs-assets\/.*/,
+        /android\/.*/,
+        /ios\/.*/
+    ]);
   },
 };
