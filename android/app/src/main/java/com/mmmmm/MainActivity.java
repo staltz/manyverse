54f1e38ed9b5d561ae5775a4c7a166c6503b1663
Andre Staltz <andre@staltz.com>
2018-06-19 15:23:58 +0300

    ux: add splash screen

--- android/app/src/main/java/com/mmmmm/MainActivity.java
+++ android/app/src/main/java/com/mmmmm/MainActivity.java
@@ -1,7 +1,19 @@
 package com.mmmmm;
 
+import android.graphics.Color;
+import android.widget.LinearLayout;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
 
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        view.setBackgroundColor(Color.parseColor("#3b5bdb"));
+        LinearLayout.LayoutParams lp =
+            new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 315);
+        view.setLayoutParams(lp);
+        return view;
+    }
 }
