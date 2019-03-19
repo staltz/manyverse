staltz/manyverse

commit 32e580aeb848dd2563acca702114a8c22d4f3120

dx: refactor to delete about.obs depject module

src/frontend/drivers/ssb.ts
@@ -16,8 +16,6 @@ import {
 } from 'ssb-typescript';
 import {isMsg, isRootPostMsg, isReplyPostMsg} from 'ssb-typescript/utils';
 import {Thread as ThreadData} from 'ssb-threads/types';
-import aboutSyncOpinion from '../../ssb/opinions/about/sync';
-import aboutOpinion = require('../../ssb/opinions/about/obs');
 import makeKeysOpinion from '../../ssb/opinions/keys';
 import sbotOpinion from '../../ssb/opinions/sbot';
 import gossipOpinion from '../../ssb/opinions/gossip';
@@ -35,6 +33,7 @@ const pull = require('pull-stream');
 const cat = require('pull-cat');
 const ssbKeys = require('react-native-ssb-client-keys');
 const depjectCombine = require('depject');
+const colorHash = new (require('color-hash'))();
 
 export type MsgAndExtras<C = Content> = Msg<C> & {
   value: {
@@ -165,6 +164,30 @@ function augmentPeersWithExtras(api: any) {
       peers.push(peer);
     }
     cb(null, peers);
+  };
+}
+
+function getProfileAbout(api: any) {
+  return (id: FeedId, cb: Callback<any>) => {
+    const aboutSocialValue = api.sbot.async.aboutSocialValue[0];
+    const nameOpts = {key: 'name', dest: id};
+    aboutSocialValue(nameOpts, (e1: any, nameResult: string) => {
+      if (e1) return cb(e1);
+      const name = nameResult || shortFeedId(id);
+      const avatarOpts = {key: 'image', dest: id};
+      aboutSocialValue(avatarOpts, (e2: any, val: any) => {
+        if (e2) return cb(e2);
+        let image: string | null = val;
+        if (!!val && typeof val === 'object' && val.link) image = val.link;
+        const imageUrl: string | null = image ? blobIdToUrl(image) : null;
+        const descriptionOpts = {key: 'description', dest: id};
+        aboutSocialValue(descriptionOpts, (e3: any, descResult: string) => {
+          if (e3) return cb(e3);
+          const description = typeof descResult === 'string' ? descResult : '';
+          cb(null, {name, imageUrl, description});
+        });
+      });
+    });
   };
 }
 
@@ -409,24 +432,13 @@ export class SSBSource {
   public profileAbout$(id: FeedId): Stream<About & {id: FeedId}> {
     return this.api$
       .map(api => {
-        const name$ = api.about.obs.name[0](id) as Stream<string>;
-        const color$ = api.about.obs.color[0](id) as Stream<string>;
-        const imageUrl$ = api.about.obs.imageUrl[0](id) as Stream<string>;
-        const following$ = api.contact.obs.tristate[0](
-          api.keys.sync.id[0](),
-          id,
-        ) as Stream<boolean | null>;
-        const description$ = api.about.obs.description[0](id) as Stream<string>;
+        const selfId = api.keys.sync.id[0]();
+        const color = colorHash.hex(id);
+        const misc$ = xsFromCallback(getProfileAbout(api))(id);
+        const following$ = api.contact.obs.tristate[0](selfId, id);
         return xs
-          .combine(name$, color$, description$, following$, imageUrl$)
-          .map(([name, color, description, following, imageUrl]) => ({
-            name,
-            color,
-            description,
-            imageUrl,
-            following,
-            id,
-          }));
+          .combine(misc$, following$)
+          .map(([misc, following]) => ({...misc, following, color, id} as any));
       })
       .flatten();
   }
@@ -553,12 +565,10 @@ export function ssbDriver(sink: Stream<Req>): SSBSource {
     .map(keys => {
       return depjectCombine([
         publishHookOpinion,
-        aboutSyncOpinion,
         configOpinion,
         makeKeysOpinion(keys),
         sbotOpinion,
         gossipOpinion,
-        aboutOpinion,
         contactOpinion,
       ]);
     })

src/ssb/opinions/about/obs.js
@@ -1,51 +1,0 @@
-/* Copyright (C) 2018-2019 The Manyverse Authors.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-import xs from 'xstream';
-var pull = require('pull-stream');
-var nest = require('depnest');
-var ref = require('ssb-ref');
-var blobIdToUrl = require('ssb-serve-blobs/id-to-url');
-var colorHash = new (require('color-hash'))();
-
-exports.needs = nest({
-  'sbot.pull.aboutSocialValueStream': 'first',
-  'about.sync.shortFeedId': 'first',
-  'keys.sync.id': 'first',
-});
-
-exports.gives = nest({
-  'about.obs': ['name', 'description', 'image', 'imageUrl', 'color'],
-});
-
-exports.create = function(api) {
-  return nest({
-    'about.obs': {
-      name: id => socialValue$(id, 'name', api.about.sync.shortFeedId(id)),
-      description: id => socialValue$(id, 'description'),
-      image: id => socialValue$(id, 'image'),
-      imageUrl: id =>
-        socialValue$(id, 'image')
-          .map(x => (!!x && typeof x === 'object' && x.link ? x.link : x))
-          .map(blobId => (blobId ? blobIdToUrl(blobId) : null)),
-      color: id => xs.of(colorHash.hex(id)).remember(),
-    },
-  });
-
-  function socialValue$(id, key, defaultValue) {
-    if (!ref.isLink(id)) throw new Error('About requires an ssb ref!');
-    return xs.createWithMemory({
-      start(listener) {
-        listener.next(defaultValue);
-        this.sink = pull.drain(listener.next.bind(listener));
-        pull(api.sbot.pull.aboutSocialValueStream({key, dest: id}), this.sink);
-      },
-      stop() {
-        if (this.sink) this.sink.abort(true);
-      },
-    });
-  }
-};

src/ssb/opinions/about/sync.ts
@@ -1,17 +1,0 @@
-/* Copyright (C) 2018-2019 The Manyverse Authors.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-import {shortFeedId} from '../../from-ssb';
-const nest = require('depnest');
-
-const shortFeedIdOpinion = {
-  gives: nest('about.sync.shortFeedId'),
-  create: (api: any) => {
-    return nest('about.sync.shortFeedId', shortFeedId);
-  },
-};
-
-export default shortFeedIdOpinion;

commit 78de9935694cb4b336e875a9e49a090b29ca1658

dx: fix updating of the profile after an edit
Fixes some bugs that commit 32e580aeb848dd2563acca702114a8c22d4f3120 introduced

src/frontend/components/messages/AboutMessage.ts
@@ -88,7 +88,7 @@ function renderWithNameDesc(name: string | null, msg: Msg<About>) {
         h(Text, {style: styles.followed}, '" and the description: '),
       ]),
     ]),
-    Markdown(msg.value.content.description as string),
+    Markdown(msg.value.content.description || ''),
     h(View, {style: styles.row}, [
       h(Text, {style: styles.timestamp}, [
         h(HumanTime as any, {time: msg.value.timestamp}),
@@ -103,7 +103,7 @@ function renderWithDesc(name: string | null, msg: Msg<About>) {
       h(Text, accountTextProps, authorName(name, msg)),
       h(Text, {style: styles.followed}, ' has a new description: '),
     ]),
-    Markdown(msg.value.content.description as string),
+    Markdown(msg.value.content.description || ''),
     h(View, {style: styles.row}, [
       h(Text, {style: styles.timestamp}, [
         h(HumanTime as any, {time: msg.value.timestamp}),

src/frontend/drivers/ssb.ts
@@ -27,8 +27,7 @@ import xsFromCallback from 'xstream-from-callback';
 import runAsync = require('promisify-tuple');
 import xsFromPullStream from 'xstream-from-pull-stream';
 import {Readable, Callback} from '../../typings/pull-stream';
-import {shortFeedId} from '../../ssb/from-ssb';
-const blobIdToUrl = require('ssb-serve-blobs/id-to-url');
+import {shortFeedId, imageToImageUrl} from '../../ssb/from-ssb';
 const pull = require('pull-stream');
 const cat = require('pull-cat');
 const ssbKeys = require('react-native-ssb-client-keys');
@@ -96,9 +95,7 @@ function mutateMsgWithLiveExtras(api: any) {
       const avatarOpts = {key: 'image', dest: msg.value.author};
       aboutSocialValue(avatarOpts, (e2: any, val: any) => {
         if (e2) return cb(e2);
-        let image: string | null = val;
-        if (!!val && typeof val === 'object' && val.link) image = val.link;
-        const imageUrl: string | null = image ? blobIdToUrl(image) : null;
+        const imageUrl = imageToImageUrl(val);
         const likes = xsFromPullStream(
           api.sbot.pull.voterStream[0](msg.key),
         ).startWith([]);
@@ -135,9 +132,7 @@ function augmentPeerWithExtras(api: any) {
       const avatarOpts = {key: 'image', dest: kv[1].key};
       aboutSocialValue(avatarOpts, (e2: any, val: any) => {
         if (e2) return cb(e2);
-        let image: string | null = val;
-        if (!!val && typeof val === 'object' && val.link) image = val.link;
-        const imageUrl: string | null = image ? blobIdToUrl(image) : null;
+        const imageUrl = imageToImageUrl(val);
         const peer = {...kv[1], name, imageUrl};
         cb(null, peer);
       });
@@ -169,30 +164,6 @@ function augmentPeersWithExtras(api: any) {
       peers.push(peer);
     }
     cb(null, peers);
-  };
-}
-
-function getProfileAbout(api: any) {
-  return (id: FeedId, cb: Callback<any>) => {
-    const aboutSocialValue = api.sbot.async.aboutSocialValue[0];
-    const nameOpts = {key: 'name', dest: id};
-    aboutSocialValue(nameOpts, (e1: any, nameResult: string) => {
-      if (e1) return cb(e1);
-      const name = nameResult || shortFeedId(id);
-      const avatarOpts = {key: 'image', dest: id};
-      aboutSocialValue(avatarOpts, (e2: any, val: any) => {
-        if (e2) return cb(e2);
-        let image: string | null = val;
-        if (!!val && typeof val === 'object' && val.link) image = val.link;
-        const imageUrl: string | null = image ? blobIdToUrl(image) : null;
-        const descriptionOpts = {key: 'description', dest: id};
-        aboutSocialValue(descriptionOpts, (e3: any, descResult: string) => {
-          if (e3) return cb(e3);
-          const description = typeof descResult === 'string' ? descResult : '';
-          cb(null, {name, imageUrl, description});
-        });
-      });
-    });
   };
 }
 
@@ -439,14 +410,61 @@ export class SSBSource {
       .map(api => {
         const selfId = api.keys.sync.id[0]();
         const color = colorHash.hex(id);
-        const misc$ = xsFromCallback(getProfileAbout(api))(id);
+        const aboutSocialValue = api.sbot.async.aboutSocialValue[0];
+        const aboutSocialValue$ = xsFromCallback(aboutSocialValue);
+        const name$ = aboutSocialValue$({key: 'name', dest: id});
+        const imageUrl$ = aboutSocialValue$({key: 'image', dest: id}).map(
+          imageToImageUrl,
+        );
+        const description$ = aboutSocialValue$({key: 'description', dest: id});
         const following$ = api.contact.obs.tristate[0](selfId, id);
         const followsYou$ = api.contact.obs.tristate[0](id, selfId);
         return xs
-          .combine(misc$, following$, followsYou$)
+          .combine(name$, imageUrl$, description$, following$, followsYou$)
           .map(
-            ([misc, following, followsYou]) =>
-              ({...misc, following, followsYou, color, id} as AboutAndExtras),
+            ([name, imageUrl, description, following, followsYou]) =>
+              ({
+                id,
+                name,
+                color,
+                imageUrl,
+                description,
+                following,
+                followsYou,
+              } as AboutAndExtras),
+          );
+      })
+      .flatten();
+  }
+
+  public profileAboutLive$(id: FeedId): Stream<AboutAndExtras> {
+    return this.api$
+      .map(api => {
+        const selfId = api.keys.sync.id[0]();
+        const color = colorHash.hex(id);
+        const aboutPS = api.sbot.pull.aboutSocialValueStream[0];
+        const name$ = xsFromPullStream(aboutPS({key: 'name', dest: id}));
+        const imageUrl$ = xsFromPullStream(
+          aboutPS({key: 'image', dest: id}),
+        ).map(imageToImageUrl);
+        const description$ = xsFromPullStream(
+          aboutPS({key: 'description', dest: id}),
+        );
+        const following$ = api.contact.obs.tristate[0](selfId, id);
+        const followsYou$ = api.contact.obs.tristate[0](id, selfId);
+        return xs
+          .combine(name$, imageUrl$, description$, following$, followsYou$)
+          .map(
+            ([name, imageUrl, description, following, followsYou]) =>
+              ({
+                id,
+                name,
+                color,
+                imageUrl,
+                description,
+                following,
+                followsYou,
+              } as AboutAndExtras),
           );
       })
       .flatten();

src/frontend/screens/biography/index.ts
@@ -14,7 +14,7 @@ import {topBar, Sinks as TBSinks} from './top-bar';
 import {AboutAndExtras} from '../../drivers/ssb';
 import view from './view';
 
-export type Props = {about: AboutAndExtras & {description: string}};
+export type Props = {about: AboutAndExtras};
 
 export type Sources = {
   props: Stream<Props>;

src/frontend/screens/biography/view.ts
@@ -35,7 +35,9 @@ export default function view(
             })
           : (null as any),
 
-        h(View, {style: styles.bioArea}, [Markdown(state.about.description)]),
+        h(View, {style: styles.bioArea}, [
+          Markdown(state.about.description || ''),
+        ]),
       ]),
     ]);
   });

src/frontend/screens/drawer/model.ts
@@ -19,7 +19,7 @@ export type State = {
 export default function model(ssbSource: SSBSource): Stream<Reducer<State>> {
   const initAboutReducer$ = ssbSource.selfFeedId$
     .take(1)
-    .map(selfFeedId => ssbSource.profileAbout$(selfFeedId))
+    .map(selfFeedId => ssbSource.profileAboutLive$(selfFeedId))
     .flatten()
     .map(
       about =>

src/frontend/screens/profile/model.ts
@@ -51,7 +51,7 @@ export default function model(
   );
 
   const about$ = props$
-    .map(props => ssbSource.profileAbout$(props.feedId))
+    .map(props => ssbSource.profileAboutLive$(props.feedId))
     .flatten();
 
   const updateAboutReducer$ = about$.map(

src/ssb/from-ssb.ts
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import {Msg, FeedId} from 'ssb-typescript';
+const blobIdToUrl = require('ssb-serve-blobs/id-to-url');
 
 export function authorName(name: string | null, msg: Msg): string {
   return name || shortFeedId(msg.value.author);
@@ -12,4 +13,11 @@ export function authorName(name: string | null, msg: Msg): string {
 
 export function shortFeedId(feedId: FeedId): string {
   return feedId.slice(0, 11) + '\u2026';
+}
+
+export function imageToImageUrl(val: any) {
+  let image: string | null = val;
+  if (!!val && typeof val === 'object' && val.link) image = val.link;
+  const imageUrl: string | null = image ? blobIdToUrl(image) : null;
+  return imageUrl;
 }