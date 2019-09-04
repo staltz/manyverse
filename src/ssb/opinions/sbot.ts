03209b513b0381546488ae791ede03053869c67d
Andre Staltz <andre@staltz.com>
2019-09-04 15:53:04 +0300

    dx: refactor, simplify sbot depject

--- src/ssb/opinions/sbot.ts
+++ src/ssb/opinions/sbot.ts
@@ -4,17 +4,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import xs from 'xstream';
 import {Readable} from '../../typings/pull-stream';
 import {manifest} from '../manifest-client';
 import {startSyncingNotifications} from '../syncing-notifications';
 import {AboutContent, FeedId, MsgId} from 'ssb-typescript';
 const pull = require('pull-stream');
 const Notify = require('pull-notify');
-const ref = require('ssb-ref');
-const Reconnect = require('pull-reconnect');
+const Ref = require('ssb-ref');
 const nest = require('depnest');
-const createFeed = require('ssb-feed');
+const Defer = require('pull-defer');
 const ssbKeys = require('react-native-ssb-client-keys');
 const muxrpc = require('muxrpc');
 const nodejs = require('nodejs-mobile-react-native');
@@ -78,69 +76,36 @@ function toSodiumKeys(keys: any) {
 const create = (api: any) => {
   const keys = api.keys.sync.load();
 
-  let sbot: any = null;
-  const sbot$ = xs.createWithMemory<any>();
   const DUNBAR = 150;
   const socialValueCache = {
     name: new QuickLRU({maxSize: DUNBAR}) as Map<FeedId, string>,
     image: new QuickLRU({maxSize: DUNBAR}) as Map<FeedId, any>,
   };
 
-  const rec = Reconnect((isConn: any) => {
-    function notify(value?: any) {
-      isConn(value);
-    }
-
+  const sbotP = new Promise<any>((resolve, reject) => {
     const ms = MultiServer([
       [
         rnChannelPlugin(nodejs.channel),
-        noAuthPlugin({
-          keys: toSodiumKeys(keys),
-        }),
+        noAuthPlugin({keys: toSodiumKeys(keys)}),
       ],
     ]);
-
     const address = 'channel~noauth:' + keys.public.replace('.ed25519', '');
-
     ms.client(address, (err: any, stream: Readable<any>) => {
       if (err) {
-        return notify(err);
+        reject(err);
+      } else {
+        const client = muxrpc(manifest, null)();
+        pull(stream, client.createStream(), stream);
+        resolve(client);
       }
-      const client = muxrpc(manifest, null)();
-      pull(stream, client.createStream(), stream);
-      sbot = client;
-      sbot$.shamefullySendNext(sbot);
-      sbot.on('closed', () => {
-        sbot = null;
-        sbot$.shamefullySendNext(sbot);
-        notify(new Error('closed'));
-      });
-
-      notify();
     });
   });
 
-  const internal = {
-    getLatest: rec.async((id: any, cb: any) => {
-      sbot.getLatest(id, cb);
-    }),
-    add: rec.async((msg: any, cb: any) => {
-      sbot.add(msg, cb);
-    }),
-  };
-
-  const feed = createFeed(internal, keys, {remote: true});
-
   const syncingStream = Notify();
-  sbot$
-    .filter(s => !!s)
-    .take(1)
-    .subscribe({
-      next: ssbClient => {
-        pull(ssbClient.syncing.stream(), pull.drain(syncingStream));
-        startSyncingNotifications(syncingStream.listen());
-      },
-    });
+  sbotP.then(ssbClient => {
+    pull(ssbClient.syncing.stream(), pull.drain(syncingStream));
+    startSyncingNotifications(syncingStream.listen());
+  });
 
   return {
     sbot: {
@@ -152,154 +117,241 @@ const create = (api: any) => {
         },
       },
       async: {
-        ssb: rec.async((cb: any) => {
-          cb(null, sbot);
-        }),
-        get: rec.async((key: any, cb: any) => {
+        ssb: (cb: any) => {
+          sbotP.then(sbot => cb(null, sbot));
+        },
+        get: (key: any, cb: any) => {
           if (typeof cb !== 'function') {
             throw new Error('cb must be function');
           }
-          sbot.get(key, (err: any, value: any) => {
-            if (err) return cb(err);
-            cb(null, value);
+          sbotP.then(sbot => {
+            sbot.get(key, (err: any, value: any) => {
+              if (err) return cb(err);
+              cb(null, value);
+            });
           });
-        }),
-        progress: rec.async((cb: any) => {
-          sbot.progress(cb);
-        }),
-        publish: rec.async((content: any, cb: any) => {
+        },
+        progress: (cb: any) => {
+          sbotP.then(sbot => {
+            sbot.progress(cb);
+          });
+        },
+        publish: (content: any, cb: any) => {
           if (!content) {
             if (cb) cb(new Error('invalid (falsy) content'));
             return;
           }
-          if (sbot) {
-            // instant updating of interface (just incase sbot is busy)
-            runHooks({
-              publishing: true,
-              timestamp: Date.now(),
-              value: {
+          sbotP.then(sbot => {
+            if (sbot) {
+              // instant updating of interface (just incase sbot is busy)
+              runHooks({
+                publishing: true,
                 timestamp: Date.now(),
-                author: keys.id,
-                content,
-              },
-            });
-          }
-          if (content.recps) {
-            content = ssbKeys.box(
-              content,
-              content.recps.map((e: any) => {
-                return ref.isFeed(e) ? e : e.link;
-              }),
-            );
-          } else if (content.mentions) {
-            content.mentions.forEach((mention: any) => {
-              if (ref.isBlob(mention.link)) {
-                sbot.blobs.push(mention.link, (err: any) => {
-                  if (err) console.error(err);
-                });
-              }
-            });
-          }
-          feed.add(content, (err: any, msg: any) => {
-            if (err) console.error(err);
-            if (cb) cb(err, msg);
-          });
-        }),
-        publishAbout: rec.async((content: AboutContent, cb: any) => {
-          if (content.image && !ref.isBlobId(content.image[0])) {
-            sbot.blobsFromPath.add(content.image, (err: any, hash: string) => {
-              if (err) return console.error(err);
-              content.image = hash;
-              feed.add(content, (err2: any, msg: any) => {
-                if (err2) console.error(err2);
-                if (cb) cb(err2, msg);
+                value: {
+                  timestamp: Date.now(),
+                  author: keys.id,
+                  content,
+                },
               });
-            });
-          } else {
-            feed.add(content, (err: any, msg: any) => {
+            }
+            if (content.recps) {
+              content = ssbKeys.box(
+                content,
+                content.recps.map((e: any) => {
+                  return Ref.isFeed(e) ? e : e.link;
+                }),
+              );
+            } else if (content.mentions) {
+              content.mentions.forEach((mention: any) => {
+                if (Ref.isBlob(mention.link)) {
+                  sbot.blobs.push(mention.link, (err: any) => {
+                    if (err) console.error(err);
+                  });
+                }
+              });
+            }
+            sbot.publish(content, (err: any, msg: any) => {
               if (err) console.error(err);
               if (cb) cb(err, msg);
             });
-          }
-        }),
-        createDhtInvite: rec.async((cb: any) => {
-          sbot.dhtInvite.create(cb);
-        }),
-        isFollowing: rec.async((opts: any, cb: any) => {
-          sbot.friends.isFollowing(opts, cb);
-        }),
-        isBlocking: rec.async((opts: any, cb: any) => {
-          sbot.friends.isBlocking(opts, cb);
-        }),
-        aboutSocialValue: rec.async((opts: any, cb: any) => {
+          });
+        },
+        publishAbout: (content: AboutContent, cb: any) => {
+          sbotP.then(sbot => {
+            if (content.image && !Ref.isBlobId(content.image[0])) {
+              sbot.blobsFromPath.add(
+                content.image,
+                (err: any, hash: string) => {
+                  if (err) return console.error(err);
+                  content.image = hash;
+                  sbot.publish(content, (err2: any, msg: any) => {
+                    if (err2) console.error(err2);
+                    if (cb) cb(err2, msg);
+                  });
+                },
+              );
+            } else {
+              sbot.publish(content, (err: any, msg: any) => {
+                if (err) console.error(err);
+                if (cb) cb(err, msg);
+              });
+            }
+          });
+        },
+        createDhtInvite: (cb: any) => {
+          sbotP.then(sbot => {
+            sbot.dhtInvite.create(cb);
+          });
+        },
+        isFollowing: (opts: any, cb: any) => {
+          sbotP.then(sbot => {
+            sbot.friends.isFollowing(opts, cb);
+          });
+        },
+        isBlocking: (opts: any, cb: any) => {
+          sbotP.then(sbot => {
+            sbot.friends.isBlocking(opts, cb);
+          });
+        },
+        aboutSocialValue: (opts: any, cb: any) => {
           if (opts.key === 'name' || opts.key === 'image') {
             const cache = socialValueCache[opts.key];
             const author = opts.dest;
             if (cache.has(author)) {
               return cb(null, cache.get(author));
             } else {
-              sbot.about.socialValue(opts, (err: any, val: string) => {
-                if (!err && !!val) cache.set(author, val);
-                cb(err, val);
+              sbotP.then(sbot => {
+                sbot.about.socialValue(opts, (err: any, val: string) => {
+                  if (!err && !!val) cache.set(author, val);
+                  cb(err, val);
+                });
               });
             }
           } else {
-            sbot.about.socialValue(opts, cb);
+            sbotP.then(sbot => {
+              sbot.about.socialValue(opts, cb);
+            });
           }
-        }),
+        },
       },
       pull: {
-        syncing: rec.source(() => {
+        syncing: () => {
           return syncingStream.listen();
-        }),
-        backlinks: rec.source((query: any) => {
-          return sbot.backlinks.read(query);
-        }),
-        voterStream: rec.source((msgId: MsgId) => {
-          return sbot.votes.voterStream(msgId);
-        }),
-        userFeed: rec.source((opts: any) => {
-          return sbot.createUserStream(opts);
-        }),
-        thread: rec.source((opts: any) => {
-          return sbot.threads.thread(opts);
-        }),
-        publicThreads: rec.source((opts: any) => {
-          return sbot.threads.public(opts);
-        }),
-        publicUpdates: rec.source((opts: any) => {
-          return sbot.threads.publicUpdates(opts);
-        }),
-        profileThreads: rec.source((opts: any) => {
-          return sbot.threads.profile(opts);
-        }),
-        feed: rec.source((opts: any) => {
-          return sbot.createFeedStream(opts);
-        }),
-        links: rec.source((query: any) => {
-          return sbot.links(query);
-        }),
-        hostingDhtInvites: rec.source(() => {
-          return sbot.dhtInvite.hostingInvites();
-        }),
-        connPeers: rec.source(() => {
-          return sbot.conn.peers();
-        }),
-        connStagedPeers: rec.source(() => {
-          return sbot.conn.stagedPeers();
-        }),
-        claimingDhtInvites: rec.source(() => {
-          return sbot.dhtInvite.claimingInvites();
-        }),
-        aboutSocialValueStream: rec.source((opts: any) => {
-          return sbot.about.socialValueStream(opts);
-        }),
-        nearbyBluetoothPeers: rec.source((refreshInterval: number) => {
-          return sbot.bluetooth.nearbyScuttlebuttDevices(refreshInterval);
-        }),
-        bluetoothScanState: rec.source(() => {
-          return sbot.bluetooth.bluetoothScanState();
-        }),
+        },
+        backlinks: (query: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.backlinks.read(query));
+          });
+          return deferred;
+        },
+        voterStream: (msgId: MsgId) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.votes.voterStream(msgId));
+          });
+          return deferred;
+        },
+        userFeed: (opts: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.createUserStream(opts));
+          });
+          return deferred;
+        },
+        thread: (opts: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.threads.thread(opts));
+          });
+          return deferred;
+        },
+        publicThreads: (opts: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.threads.public(opts));
+          });
+          return deferred;
+        },
+        publicUpdates: (opts: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.threads.publicUpdates(opts));
+          });
+          return deferred;
+        },
+        profileThreads: (opts: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.threads.profile(opts));
+          });
+          return deferred;
+        },
+        feed: (opts: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.createFeedStream(opts));
+          });
+          return deferred;
+        },
+        links: (query: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.links(query));
+          });
+          return deferred;
+        },
+        hostingDhtInvites: () => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.dhtInvite.hostingInvites());
+          });
+          return deferred;
+        },
+        connPeers: () => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.conn.peers());
+          });
+          return deferred;
+        },
+        connStagedPeers: () => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.conn.stagedPeers());
+          });
+          return deferred;
+        },
+        claimingDhtInvites: () => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.dhtInvite.claimingInvites());
+          });
+          return deferred;
+        },
+        aboutSocialValueStream: (opts: any) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.about.socialValueStream(opts));
+          });
+          return deferred;
+        },
+        nearbyBluetoothPeers: (refreshInterval: number) => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(
+              sbot.bluetooth.nearbyScuttlebuttDevices(refreshInterval),
+            );
+          });
+          return deferred;
+        },
+        bluetoothScanState: () => {
+          const deferred = Defer.source();
+          sbotP.then(sbot => {
+            deferred.resolve(sbot.bluetooth.bluetoothScanState());
+          });
+          return deferred;
+        },
       },
     },
   };
