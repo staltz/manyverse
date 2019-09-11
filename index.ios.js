fb17cff15d3f8f5200d0730cf27b48376b0f16eb
Andre Staltz <andre@staltz.com>
2019-09-11 18:54:36 +0300

    dx: prepare iOS config files for development

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,17 @@
-/**
- * IN PROGRESS keep and eye on https://github.com/luandro/ssb-nodejs-mobile for tests.
- */
+/* Copyright (C) 2018-2019 The Manyverse Authors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import 'react-native-ssb-shims';
+import {run} from 'cycle-native-navigation';
+import {
+  screens,
+  drivers,
+  layout,
+  defaultNavOptions,
+} from './lib/frontend/index';
+// import './snoopy'; // Log and debug the React Native JS<-->Native Bridge
+
+run(screens, drivers, layout, defaultNavOptions);
