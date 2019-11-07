cdab146fafb4ebd648234b5e22bfa0d722c96814
Andre Staltz <andre@staltz.com>
2019-11-07 17:00:38 +0200

    dx: rebase codebase with master branch

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -4,20 +4,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const rnBridge = require('rn-bridge');
-const restore = require('./restore');
-
 // Setup initial communication with the frontend, to create or restore identity
-rnBridge.channel.on('identity', (request: string) => {
-  const startSSB = () => require('./ssb');
-  let response;
-  if (request === 'CREATE' || request === 'USE') {
-    startSSB();
-    response = 'IDENTITY_READY';
-  } else if (request.startsWith('RESTORE:')) {
-    const words = request.split('RESTORE: ')[1].trim();
-    response = restore(words);
-    if (response === 'IDENTITY_READY') startSSB();
-  }
-  rnBridge.channel.post('identity', response);
-});
+if (process.env.MANYVERSE_PLATFORM === 'mobile') {
+  const rnBridge = require('rn-bridge');
+  const restore = require('./restore');
+  rnBridge.channel.on('identity', (request: string) => {
+    const startSSB = () => require('./ssb');
+    let response;
+    if (request === 'CREATE' || request === 'USE') {
+      startSSB();
+      response = 'IDENTITY_READY';
+    } else if (request.startsWith('RESTORE:')) {
+      const words = request.split('RESTORE: ')[1].trim();
+      response = restore(words);
+      if (response === 'IDENTITY_READY') startSSB();
+    }
+    rnBridge.channel.post('identity', response);
+  });
+} else {
+  require('./ssb');
+}
