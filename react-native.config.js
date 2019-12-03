ad0de0304a739c2b80bc92920b097fc903891c7c
Andre Staltz <andre@staltz.com>
2019-12-03 14:06:09 +0200

    dx: [ios] install react-native-styled-dialogs

--- react-native.config.js
+++ react-native.config.js
@@ -23,5 +23,11 @@ module.exports = {
         android: null,
       },
     },
+    'react-native-styled-dialogs': {
+      platforms: {
+        // On Android, we use react-native-dialogs, so we don't need this
+        android: null,
+      },
+    },
   },
 };
