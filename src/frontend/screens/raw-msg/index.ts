69929e416d4ccc3d9aaa28213923fc662bb11c9b
Andre Staltz <andre@staltz.com>
2019-12-16 15:12:40 +0200

    dx: [ios] fix RawMsg screen

--- src/frontend/screens/raw-msg/index.ts
+++ src/frontend/screens/raw-msg/index.ts
@@ -4,16 +4,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {Stream} from 'xstream';
-import {Command, PopCommand, NavSource} from 'cycle-native-navigation';
+import xs, {Stream} from 'xstream';
+import {Command, NavSource} from 'cycle-native-navigation';
 import {ReactSource, h} from '@cycle/react';
 import {ReactElement} from 'react';
-import {ScrollView, Text, StyleSheet} from 'react-native';
-import {getStatusBarHeight} from 'react-native-status-bar-height';
+import isolate from '@cycle/isolate';
+import {ScrollView, View, Text, StyleSheet} from 'react-native';
 import {SSBSource, MsgAndExtras} from '../../drivers/ssb';
-import {Dimensions} from '../../global-styles/dimens';
 import {Palette} from '../../global-styles/palette';
 import {Typography} from '../../global-styles/typography';
+import {topBar, Sinks as TBSinks} from './top-bar';
 
 export type Props = {
   msg: MsgAndExtras;
@@ -33,21 +33,19 @@ export type Sinks = {
 
 export const navOptions = {
   topBar: {
-    visible: true,
-    drawBehind: false,
-    height: Dimensions.toolbarHeight,
-    paddingTop: getStatusBarHeight(true),
-    title: {
-      text: 'Raw message',
-    },
-    backButton: {
-      icon: require('../../../../images/icon-arrow-left.png'),
-      visible: true,
-    },
+    visible: false,
+    height: 0,
   },
 };
 
 export const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: Palette.backgroundVoid,
+    flexDirection: 'column',
+  },
+
   container: {
     flex: 1,
     backgroundColor: Palette.backgroundHackerVoid,
@@ -62,22 +60,33 @@ export const styles = StyleSheet.create({
 });
 
 export function rawMessage(sources: Sources): Sinks {
-  const vdom$ = sources.props.map(props =>
-    h(ScrollView, {style: styles.container}, [
-      h(
-        Text,
-        {style: styles.content, selectable: true},
-        JSON.stringify(
-          props.msg,
-          (key, value) => (key === '_$manyverse$metadata' ? undefined : value),
-          2,
-        ),
-      ),
-    ]),
-  );
-  const command$ = sources.navigation.backPress().mapTo({
-    type: 'pop',
-  } as PopCommand);
+  const topBarSinks: TBSinks = isolate(topBar, 'topBar')(sources);
+
+  const vdom$ = xs
+    .combine(topBarSinks.screen, sources.props)
+    .map(([topBarVDOM, props]) =>
+      h(View, {style: styles.screen}, [
+        topBarVDOM,
+        h(ScrollView, {style: styles.container}, [
+          h(
+            Text,
+            {style: styles.content, selectable: true},
+            JSON.stringify(
+              props.msg,
+              (key, value) =>
+                key === '_$manyverse$metadata' ? undefined : value,
+              2,
+            ),
+          ),
+        ]),
+      ]),
+    );
+
+  const command$ = xs
+    .merge(sources.navigation.backPress(), topBarSinks.back)
+    .mapTo({
+      type: 'pop',
+    } as Command);
 
   return {
     screen: vdom$,
---
+++ src/frontend/screens/raw-msg/top-bar/index.ts
@@ -0,0 +1,62 @@
+/* Copyright (C) 2018-2019 The Manyverse Authors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import xs, {Stream} from 'xstream';
+import {ReactSource} from '@cycle/react';
+import {h} from '@cycle/react';
+import {ReactElement} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+import {Palette} from '../../../global-styles/palette';
+import {Dimensions} from '../../../global-styles/dimens';
+import {Typography} from '../../../global-styles/typography';
+import HeaderBackButton from '../../../components/HeaderBackButton';
+
+export type Sources = {
+  screen: ReactSource;
+};
+
+export type Sinks = {
+  screen: Stream<ReactElement<any>>;
+  back: Stream<any>;
+};
+
+export const styles = StyleSheet.create({
+  container: {
+    height: Dimensions.toolbarHeight,
+    paddingTop: getStatusBarHeight(true),
+    alignSelf: 'stretch',
+    backgroundColor: Palette.backgroundBrand,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: Dimensions.horizontalSpaceBig,
+  },
+
+  title: {
+    marginLeft: Dimensions.horizontalSpaceLarge,
+    fontFamily: Typography.fontFamilyReadableText,
+    color: Palette.textForBackgroundBrand,
+    fontSize: Typography.fontSizeLarge,
+    fontWeight: 'bold',
+  },
+});
+
+export function topBar(sources: Sources): Sinks {
+  const vdom$ = xs.of(
+    h(View, {style: styles.container}, [
+      HeaderBackButton('rawMsgBackButton'),
+      h(Text, {style: styles.title}, 'Raw message'),
+    ]),
+  );
+
+  const back$ = sources.screen.select('rawMsgBackButton').events('press');
+
+  return {
+    screen: vdom$,
+    back: back$,
+  };
+}
