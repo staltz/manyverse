faec9954d2cf1e35404ca606c8dde79eaa07504b
Andre Staltz <andre@staltz.com>
2020-01-07 14:56:46 +0200

    dx: update post-release scripts

--- tools/echo-ssb-post.js
+++ tools/echo-ssb-post.js
@@ -13,11 +13,12 @@ intoStream('--------------BEGIN SCUTTLEBUTT POST--------------\n')
     addStream(
       intoStream(
         '**Update from one of these sources:**\n\n' +
-          '- [Dat Installer](https://github.com/staltz/dat-installer/) (available now)\n' +
+          '- (Android) [Dat Installer](https://github.com/staltz/dat-installer/) (available now)\n' +
           '  - insert `dat://manyverse-latest.hashbase.io`\n' +
           '  - or insert `dat://520a00daf0a309bef7722b3f3338854e9da667d01e48dc7b83b118d86354d6d3`\n' +
-          '- [Play Store](https://play.google.com/store/apps/details?id=se.manyver) (available within 3h – 18h)\n' +
-          '- [F-Droid](https://f-droid.org/packages/se.manyver/) (available within 72h)\n' +
+          '- (Android) [Play Store](https://play.google.com/store/apps/details?id=se.manyver) (available within 3h – 18h)\n' +
+          '- (iOS) [App Store](https://apps.apple.com/app/manyverse/id1492321617) (available within 24h – 48h, pending on Apple)\n' +
+          '- (Android) [F-Droid](https://f-droid.org/packages/se.manyver/) (available within 72h)\n' +
           '----------------END SCUTTLEBUTT POST---------------\n',
       ),
     ),
