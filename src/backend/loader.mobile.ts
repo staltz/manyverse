fcc500047ca576b17ce5964a43ed0de8fa6ecc97
Andre Staltz <andre@staltz.com>
2020-04-28 16:34:25 +0300

    dx: add work-in-progress performance marks on the backend

--- src/backend/loader.mobile.ts
+++ src/backend/loader.mobile.ts
@@ -7,6 +7,7 @@
 import os = require('os');
 import fs = require('fs');
 import path = require('path');
+// import * as PH from 'perf_hooks';
 const rnBridge = require('rn-bridge');
 
 process.env = process.env ?? {};
@@ -54,27 +55,42 @@ process.removeAllListeners = function removeAllListeners(eventName: string) {
 };
 
 // Profile Node.js CPU usage, output to the tmp dir
-// TODO unfortunately this is still unsupported in nodejs-mobile
 if (process.env.PROFILER_NODEJS) {
-  const inspector = require('inspector');
-  const session = new inspector.Session();
-  session.connect();
-  session.post('Profiler.enable', () => {
-    session.post('Profiler.start', () => {
-      require('./index');
-      setTimeout(() => {
-        session.post('Profiler.stop', (err: any, res: any) => {
-          if (err) return console.error(err);
-          const data = res.profile || res.result;
-          const date = new Date();
-          const file = `${nodejsProjectDir}/flamechart_${date.getTime()}.json`;
-          fs.writeFile(file, JSON.stringify(data), err2 => {
-            if (err2) console.error(err2);
+  // TODO figure out how to collect this perf data, how to convert it to Chrome
+  // DevTools profile json format:
+
+  // const {performance, PerformanceObserver} = require('perf_hooks') as typeof PH;
+  // let performanceEntries = [];
+  // const obs = new PerformanceObserver(items => {
+  // performanceEntries = items.getEntries();
+  // });
+  // obs.observe({entryTypes: ['mark', 'function']});
+
+  try {
+    // TODO unfortunately this is still unsupported in nodejs-mobile
+    const inspector = require('inspector');
+    const session = new inspector.Session();
+    session.connect();
+    session.post('Profiler.enable', () => {
+      session.post('Profiler.start', () => {
+        require('./index');
+        setTimeout(() => {
+          session.post('Profiler.stop', (err: any, res: any) => {
+            if (err) return console.error(err);
+            const data = res.profile || res.result;
+            const date = new Date();
+            const file = `${nodejsProjectDir}/flamechart_${date.getTime()}.json`;
+            fs.writeFile(file, JSON.stringify(data), err2 => {
+              if (err2) console.error(err2);
+            });
           });
-        });
-      }, 20e3);
+        }, 20e3);
+      });
     });
-  });
+  } catch (err) {
+    console.error(err);
+    require('./index');
+  }
 } else {
   // Just run the backend without profiling
   require('./index');
