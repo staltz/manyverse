c7ec57d307f7a0577959cc49b6ad6d5d30cdcf63
David Gomez <dg.dave@gmail.com>
2020-06-03 19:33:54 -0400

    dx: [ios] fix quick reactions: avoid row collapse

--- src/frontend/components/messages/MessageFooter.ts
+++ src/frontend/components/messages/MessageFooter.ts
@@ -102,6 +102,11 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    ...Platform.select({
+      ios: {
+        height: 60,
+      },
+    }),
   },
 
   quickEmojiChoice: {
