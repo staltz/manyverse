8201f1d3074280f559948c72f805bbe8bfb076c0
David Gomez <dg.dave@gmail.com>
2020-06-07 13:30:01 -0400

    dx: refactor typography.ts file
    Refactor the current typography utilities to provide a better developer experience by facilitating the addition of font sizes with their correct line heights

--- src/frontend/global-styles/typography.ts
+++ src/frontend/global-styles/typography.ts
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018-2019 The Manyverse Authors.
+/* Copyright (C) 2018-2020 The Manyverse Authors.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -6,27 +6,49 @@
 
 import {Platform} from 'react-native';
 
-const BASE_SIZE = 16; // px | pt
-const SCALE = 1.125;
-const LEADING = 1.5;
+type Scale = 'tiny' | 'small' | 'normal' | 'big' | 'large' | 'larger';
 
-const scaleUp = SCALE;
-const scaleDown = 1 / SCALE;
+const FONT_SIZE = 16;
+const FONT_SCALE = 1.125;
+const LINE_SCALE = 1.5;
+const SIZES = {tiny: -2, small: -1, normal: 0, big: 1, large: 2, larger: 3};
+
+export const fontSize = (scale: Scale) =>
+  FONT_SIZE * Math.pow(FONT_SCALE, SIZES[scale]);
+
+export const lineHeight = (scale: Scale) =>
+  FONT_SIZE * Math.pow(FONT_SCALE, SIZES[scale]) * LINE_SCALE;
+
+export const fontSizeLineHeight = (scale: Scale) => ({
+  fontSize: fontSize(scale),
+  lineHeight: lineHeight(scale),
+});
 
 export const Typography = {
-  scaleUp,
-  scaleDown,
-  baseSize: BASE_SIZE,
-  baseLeading: LEADING,
-  fontSizeLarge: BASE_SIZE * scaleUp * scaleUp,
-  fontSizeBig: BASE_SIZE * scaleUp,
-  fontSizeNormal: BASE_SIZE,
-  fontSizeSmall: BASE_SIZE * scaleDown,
+  fontSizeLarger: fontSize('larger'),
+  lineHeightLarger: lineHeight('larger'),
+
+  fontSizeLarge: fontSize('large'),
+  lineHeightLarge: lineHeight('large'),
+
+  fontSizeBig: fontSize('big'),
+  lineHeightBig: lineHeight('big'),
+
+  fontSizeNormal: fontSize('normal'),
+  lineHeightNormal: lineHeight('normal'),
+
+  fontSizeSmall: fontSize('small'),
+  lineHeightSmall: lineHeight('small'),
+
+  fontSizeTiny: fontSize('tiny'),
+  lineHeightTiny: lineHeight('tiny'),
+
   fontFamilyReadableText: Platform.select({
     android: 'sans-serif-light',
     ios: 'Helvetica Neue',
     default: 'sans-serif',
   }),
+
   fontFamilyMonospace: Platform.select({
     ios: 'Courier New',
     default: 'monospace',
