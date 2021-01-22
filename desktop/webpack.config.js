8b03edbe1f102d7e16e4905ea65bbf9e9039eaa4
Jacob Karlsson <jacob.karlsson95@gmail.com>
2021-01-22 11:05:11 +0000

    dx: start using react-native-web on desktop

--- desktop/webpack.config.js
+++ desktop/webpack.config.js
@@ -7,4 +7,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+  },
 };
--- src/desktop/index.ts
+++ src/desktop/index.ts
@@ -1,22 +1,34 @@
-/* Copyright (C) 2018-2020 The Manyverse Authors.
+/* Copyright (C) 2018-2021 The Manyverse Authors.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import {run} from '@cycle/run';
-import {makeDOMDriver, div, h1, button} from '@cycle/react-dom';
+import {makeReactNativeDriver, View, Text, Button} from '@cycle/react-native';
+import {AppRegistry} from 'react-native';
 import makeClient from './ssb/client';
-const pull = require('pull-stream');
 
 function main(sources: any) {
   const inc = Symbol();
-  const inc$ = sources.react.select(inc).events('click');
+  const inc$ = sources.react.select(inc).events('press');
 
   const count$ = inc$.fold((count: number) => count + 1, 0);
 
+  makeClient().then((ssb) => {
+    ssb.whoami((err: any, {id}: {id: string}) => {
+      console.log('whoami', id);
+    });
+  });
+
   const vdom$ = count$.map((i: number) =>
-    div([h1(`Counter: ${i}`), button(inc, 'Increment')]),
+    View([
+      Text(`Counter: ${i}`),
+      Button(inc, {
+        title: 'Increment',
+        onPress: () => {},
+      }),
+    ]),
   );
 
   return {
@@ -25,46 +37,8 @@ function main(sources: any) {
 }
 
 run(main, {
-  react: makeDOMDriver(document.getElementById('app')),
+  react: makeReactNativeDriver('manyverse'),
 });
-
-function myapp() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Frontend JS activated';
-
-  makeClient().then((ssb) => {
-    pull(
-      ssb.conn.peers(),
-      pull.drain((peers: Array<any>) => {
-        element.innerHTML =
-          '<ul>' +
-          peers
-            .map((peer) => peer[1].name || peer[1].key || '?')
-            .map((name) => `<li>${name}</li>`)
-            .join('\n') +
-          '</ul>';
-      }),
-    );
-
-    pull(
-      ssb.threads.public({
-        threadMaxSize: 3,
-        allowlist: ['post', 'contact'],
-        reverse: true,
-        live: false,
-      }),
-      pull.take(3),
-      pull.drain((thread: any) => {
-        console.warn(thread);
-      }),
-    );
-
-    ssb.conn.start();
-  });
-
-  return element;
-}
-
-document.body.appendChild(myapp());
+AppRegistry.runApplication('manyverse', {
+  rootTag: document.getElementById('app'),
+});
