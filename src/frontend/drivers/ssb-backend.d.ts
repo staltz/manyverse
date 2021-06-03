593702f19fd9702cc14195c60c8661dba300c1d9
Andre Staltz <andre@staltz.com>
2021-06-03 18:14:14 +0300

    dx: refactor src/frontend/drivers/ssb-backend

--- src/frontend/drivers/ssb-backend.d.ts
+++ src/frontend/drivers/ssb-backend.d.ts
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import ios from './ssb-backend.ios';
-import android from './ssb-backend.android';
-declare var _test: typeof ios;
-declare var _test: typeof android;
-export default ios;
+import native from './ssb-backend.native';
+import web from './ssb-backend.web';
+declare var _test: typeof native;
+declare var _test: typeof web;
+export default native;
--- src/frontend/drivers/ssb-backend.ios.ts
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 2021 The Manyverse Authors.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-import backend from './ssb-backend.android';
-export default backend;
