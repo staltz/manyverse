138eaeda95f89d2070a0b8ebafc7933b2faf16d1
Andre Staltz <andre@staltz.com>
2021-08-17 23:26:09 +0300

    dx: [des] center-align Private tab in Frame

--- src/frontend/screens/central/private-tab/styles.ts
+++ src/frontend/screens/central/private-tab/styles.ts
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
 import {Dimensions as Dimens} from '../../../global-styles/dimens';
 import {Typography} from '../../../global-styles/typography';
 import {Palette} from '../../../global-styles/palette';
@@ -32,6 +32,11 @@ export const styles = StyleSheet.create({
     marginBottom: 1,
     flexDirection: 'row',
     alignItems: 'center',
+    ...Platform.select({
+      web: {
+        maxWidth: Dimens.desktopMiddleWidth.vw,
+      },
+    }),
   },
 
   singleAvatar: {
