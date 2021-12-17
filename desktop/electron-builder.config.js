de119eda3b9a59711b2e289700cea5c3e456fcb6
Andre Staltz <andre@staltz.com>
2021-12-17 13:55:09 +0200

    dx: [des] set app icon on Linux

--- desktop/electron-builder.config.js
+++ desktop/electron-builder.config.js
@@ -69,6 +69,7 @@ module.exports = {
 
   // Target-specific configurations --------------------------------------------
   linux: {
+    icon: path.join(__dirname, 'build-resources', 'linux-app-icon'),
     target: [
       {
         target: 'deb',
