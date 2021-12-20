9e8fa92877b46022bcdb37225a9fd6e2ebd34529
Andre Staltz <andre@staltz.com>
2021-12-20 16:02:53 +0200

    dx: [and] replace rn-snoopy with MessageQueue.spy

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2018-2020 The Manyverse Authors
+// SPDX-FileCopyrightText: 2018-2021 The Manyverse Authors
 //
 // SPDX-License-Identifier: MPL-2.0
 
@@ -6,6 +6,11 @@ import 'react-native-ssb-shims';
 import {run} from 'cycle-native-navigation';
 import {screens, drivers} from './lib/frontend/index';
 import {welcomeLayout, defaultNavOptions} from './lib/frontend/screens/layouts';
-// import './snoopy'; // Log and debug the React Native JS<-->Native Bridge
+// import MessageQueue from 'react-native/Libraries/BatchedBridge/MessageQueue';
 
 run(screens, drivers, welcomeLayout, defaultNavOptions);
+
+// MessageQueue.spy((msg) => {
+//   if (msg.module === 'WebSocketModule') return;
+//   console.log(msg.type, msg.module, msg.method);
+// });
