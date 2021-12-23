679e8a148143459640868cf9fb433495e4ec0b17
Andre Staltz <andre@staltz.com>
2021-12-23 16:57:40 +0200

    ux: bug fix: database glitches

--- src/backend/one-time-fixes.ts
+++ src/backend/one-time-fixes.ts
@@ -4,43 +4,108 @@
 
 import path = require('path');
 import fs = require('fs');
+import util = require('util');
 const rimraf = require('rimraf');
+const BIPF = require('bipf');
+const defaults = require('ssb-db2/defaults');
+const AAOL = require('async-append-only-log');
 
 if (!process.env.SSB_DIR) {
   throw new Error('misconfigured SSB_DIR in the backend');
 }
 const SSB_DIR = process.env.SSB_DIR;
 const KEYS_PATH = path.join(SSB_DIR, 'secret');
+const OLD_LOG_PATH = defaults.oldLogPath(SSB_DIR);
+const NEW_LOG_PATH = defaults.newLogPath(SSB_DIR);
 
-// One-time fixes for special issues.
-//
-// Sometimes things go wrong in the database and we need to reset indexes or
-// others to make the database work again, but only once. This file accounts for
-// all those cases, and each of these "issues" refers to GitLab issue codes.
+async function deleteDuplicateRecordsOnLog() {
+  const log = AAOL(NEW_LOG_PATH, {
+    blockSize: defaults.BLOCK_SIZE,
+    validateRecord: (data: Buffer) => {
+      try {
+        BIPF.decode(data, 0);
+        return true;
+      } catch (ex) {
+        return false;
+      }
+    },
+  });
 
-const ISSUE_1223 = path.join(SSB_DIR, 'issue1223');
-if (!fs.existsSync(ISSUE_1223)) {
-  rimraf.sync(path.join(SSB_DIR, 'db2'));
-  fs.closeSync(fs.openSync(ISSUE_1223, 'w'));
+  const B_KEY = Buffer.from('key');
+  const existing = new Set<string>();
+  const deletables = new Set<number>();
+
+  // Find deletables
+  await new Promise<void>((resolve) => {
+    log.stream({gt: -1}).pipe({
+      paused: false,
+      write: function (record: {value: Buffer | null; offset: number}) {
+        const buffer = record.value;
+        if (!buffer) return;
+        const pKey = BIPF.seekKey(buffer, 0, B_KEY) as number;
+        const shortKey = BIPF.decode(buffer, pKey).slice(1, 33) as string;
+        if (existing.has(shortKey)) {
+          deletables.add(record.offset);
+        } else {
+          existing.add(shortKey);
+        }
+      },
+      end: () => {
+        resolve();
+      },
+    });
+  });
+
+  const del = util.promisify(log.del);
+  for (const offset of deletables) {
+    await del(offset);
+  }
+
+  existing.clear();
+  deletables.clear();
 }
 
-const ISSUE_1328 = path.join(SSB_DIR, 'issue1328');
-if (!fs.existsSync(ISSUE_1328)) {
-  rimraf.sync(path.join(SSB_DIR, 'db2', 'indexes') + '/*.*');
-  fs.closeSync(fs.openSync(ISSUE_1328, 'w'));
+/**
+ * One-time fixes for special issues.
+ *
+ * Sometimes things go wrong in the database and we need to reset indexes or
+ * others to make the database work again, but only once. This file accounts for
+ * all those cases, and each of these "issues" refers to GitLab issue codes.
+ */
+async function oneTimeFixes() {
+  const ISSUE_1223 = path.join(SSB_DIR, 'issue1223');
+  if (!fs.existsSync(ISSUE_1223) && fs.existsSync(OLD_LOG_PATH)) {
+    rimraf.sync(path.join(SSB_DIR, 'db2'));
+    fs.closeSync(fs.openSync(ISSUE_1223, 'w'));
+  }
+
+  const ISSUE_1328 = path.join(SSB_DIR, 'issue1328');
+  if (!fs.existsSync(ISSUE_1328)) {
+    rimraf.sync(defaults.indexesPath(SSB_DIR) + '/*.*');
+    fs.closeSync(fs.openSync(ISSUE_1328, 'w'));
+  }
+
+  const ISSUE_1486 = path.join(SSB_DIR, 'issue1486');
+  if (!fs.existsSync(ISSUE_1486)) {
+    rimraf.sync(defaults.indexesPath(SSB_DIR) + '/!(*.*)');
+    fs.closeSync(fs.openSync(ISSUE_1486, 'w'));
+  }
+
+  // Fix issue #1518:
+  if (fs.existsSync(KEYS_PATH) && fs.lstatSync(KEYS_PATH).isDirectory()) {
+    const keysPathWrong = path.join(KEYS_PATH, 'secret');
+    const keysPathTmp = path.join(SSB_DIR, 'tmpsecret');
+    fs.renameSync(keysPathWrong, keysPathTmp);
+    rimraf.sync(KEYS_PATH);
+    fs.renameSync(keysPathTmp, KEYS_PATH);
+  }
+
+  const ISSUE_1628 = path.join(SSB_DIR, 'issue1628');
+  if (!fs.existsSync(ISSUE_1628)) {
+    rimraf.sync(defaults.indexesPath(SSB_DIR));
+    fs.closeSync(fs.openSync(ISSUE_1628, 'w'));
+    await deleteDuplicateRecordsOnLog();
+  }
 }
 
-const ISSUE_1486 = path.join(SSB_DIR, 'issue1486');
-if (!fs.existsSync(ISSUE_1486)) {
-  rimraf.sync(path.join(SSB_DIR, 'db2', 'indexes') + '/!(*.*)');
-  fs.closeSync(fs.openSync(ISSUE_1486, 'w'));
-}
-
-// Fix issue #1518:
-if (fs.existsSync(KEYS_PATH) && fs.lstatSync(KEYS_PATH).isDirectory()) {
-  const keysPathWrong = path.join(KEYS_PATH, 'secret');
-  const keysPathTmp = path.join(SSB_DIR, 'tmpsecret');
-  fs.renameSync(keysPathWrong, keysPathTmp);
-  rimraf.sync(KEYS_PATH);
-  fs.renameSync(keysPathTmp, KEYS_PATH);
-}
+export = oneTimeFixes;
--- src/backend/ssb.ts
+++ src/backend/ssb.ts
@@ -11,6 +11,7 @@ const makeConfig = require('ssb-config/inject');
 const SecretStack = require('secret-stack');
 import settingsUtils = require('./plugins/settingsUtils');
 import bluetoothTransport = require('./plugins/bluetooth');
+import oneTimeFixes = require('./one-time-fixes');
 
 // Make sure SSB_DIR exists
 if (!process.env.APP_DATA_DIR || !process.env.SSB_DIR) {
@@ -18,103 +19,103 @@ if (!process.env.APP_DATA_DIR || !process.env.SSB_DIR) {
 }
 if (!fs.existsSync(process.env.SSB_DIR)) mkdirp.sync(process.env.SSB_DIR);
 
-require('./one-time-fixes');
+oneTimeFixes().then(() => {
+  const KEYS_PATH = path.join(process.env.SSB_DIR!, 'secret');
+  const keys = ssbKeys.loadOrCreateSync(KEYS_PATH);
 
-const KEYS_PATH = path.join(process.env.SSB_DIR, 'secret');
-const keys = ssbKeys.loadOrCreateSync(KEYS_PATH);
+  const config = makeConfig('ssb', {
+    caps,
+    keys,
+    path: process.env.SSB_DIR,
+    db2: {
+      maxCpu: 91, // %
+      maxCpuWait: 80, // ms
+      maxCpuMaxPause: 120, // ms
+      automigrate: true,
+      dangerouslyKillFlumeWhenMigrated:
+        process.env.MANYVERSE_PLATFORM === 'mobile',
+    },
+    blobs: {
+      sympathy: 2,
+    },
+    blobsPurge: {
+      cpuMax: 90, // %
+    },
+    conn: {
+      autostart: false,
+      firewall: {
+        rejectBlocked: true,
+        rejectUnknown: true,
+      },
+    },
+    friends: {
+      hops: settingsUtils.readSync().hops ?? 2,
+      hookAuth: false, // because we use ssb-conn-firewall
+    },
+    suggest: {
+      autostart: false,
+    },
+    connections: {
+      incoming: {
+        net: [{scope: 'private', transform: 'shs', port: 26831}],
+        channel: [{scope: 'device', transform: 'noauth'}],
+        bluetooth: [{scope: 'public', transform: 'shs'}],
+        tunnel: [{scope: 'public', transform: 'shs'}],
+      },
+      outgoing: {
+        net: [{transform: 'shs'}],
+        ws: [{transform: 'shs'}],
+        bluetooth: [{scope: 'public', transform: 'shs'}],
+        tunnel: [{transform: 'shs'}],
+      },
+    },
+  });
 
-const config = makeConfig('ssb', {
-  caps,
-  keys,
-  path: process.env.SSB_DIR,
-  db2: {
-    maxCpu: 91, // %
-    maxCpuWait: 80, // ms
-    maxCpuMaxPause: 120, // ms
-    automigrate: true,
-    dangerouslyKillFlumeWhenMigrated:
-      process.env.MANYVERSE_PLATFORM === 'mobile',
-  },
-  blobs: {
-    sympathy: 2,
-  },
-  blobsPurge: {
-    cpuMax: 90, // %
-  },
-  conn: {
-    autostart: false,
-    firewall: {
-      rejectBlocked: true,
-      rejectUnknown: true,
-    },
-  },
-  friends: {
-    hops: settingsUtils.readSync().hops ?? 2,
-    hookAuth: false, // because we use ssb-conn-firewall
-  },
-  suggest: {
-    autostart: false,
-  },
-  connections: {
-    incoming: {
-      net: [{scope: 'private', transform: 'shs', port: 26831}],
-      channel: [{scope: 'device', transform: 'noauth'}],
-      bluetooth: [{scope: 'public', transform: 'shs'}],
-      tunnel: [{scope: 'public', transform: 'shs'}],
-    },
-    outgoing: {
-      net: [{transform: 'shs'}],
-      ws: [{transform: 'shs'}],
-      bluetooth: [{scope: 'public', transform: 'shs'}],
-      tunnel: [{transform: 'shs'}],
-    },
-  },
+  SecretStack()
+    // Core
+    .use(require('ssb-master'))
+    .use(require('ssb-db2'))
+    .use(require('ssb-db2/compat/db'))
+    .use(require('ssb-db2/compat/ebt'))
+    .use(require('ssb-db2/compat/log-stream'))
+    .use(require('ssb-db2/compat/history-stream'))
+    .use(require('ssb-deweird/producer'))
+    // Replication
+    .use(require('ssb-ebt')) // needs: db2/compat
+    .use(require('ssb-friends')) // needs: db2
+    .use(require('ssb-replication-scheduler')) // needs: friends, ebt
+    // Connections
+    .use(require('./plugins/multiserver-addons'))
+    .use(require('ssb-lan'))
+    .use(bluetoothTransport(keys, process.env.APP_DATA_DIR!))
+    .use(require('ssb-conn')) // needs: db2, friends, lan, bluetooth
+    .use(require('ssb-conn-firewall')) // needs: friends
+    .use(require('ssb-room-client')) // needs: conn
+    .use(require('ssb-http-auth-client')) // needs: conn
+    .use(require('ssb-http-invite-client'))
+    .use(require('ssb-invite-client')) // needs: db2, conn
+    // Queries
+    .use(require('ssb-db2/about-self')) // needs: db2
+    .use(require('ssb-suggest-lite')) // needs: db2, about-self, friends
+    .use(require('ssb-threads')) // needs: db, db2, friends
+    .use(require('ssb-db2/full-mentions')) // needs: db2
+    .use(require('ssb-search2')) // needs: db2
+    // Blobs
+    .use(require('ssb-blobs'))
+    .use(require('ssb-serve-blobs')) // needs: blobs
+    .use(require('ssb-blobs-purge')) // needs: blobs, db2/full-mentions
+    // Customizations
+    .use(require('./plugins/aboutSelf')) // needs: db2
+    .use(require('./plugins/blobsUtils')) // needs: blobs
+    .use(require('./plugins/connUtils')) // needs: conn, aboutSelf
+    .use(require('./plugins/aliasUtils')) // needs: db2
+    .use(require('./plugins/resyncUtils')) // needs: db2, connFirewall
+    .use(require('./plugins/publishUtilsBack')) // needs: db, blobs, blobsUtils
+    .use(require('./plugins/searchUtils')) // needs: db2
+    .use(require('./plugins/keysUtils'))
+    .use(settingsUtils) // needs: blobs-purge
+    .use(require('./plugins/syncing')) // needs: db2
+    .use(require('./plugins/dbUtils')) // needs: db2, syncing
+    .use(require('./plugins/votes')) // needs: db2
+    .call(null, config);
 });
-
-SecretStack()
-  // Core
-  .use(require('ssb-master'))
-  .use(require('ssb-db2'))
-  .use(require('ssb-db2/compat/db'))
-  .use(require('ssb-db2/compat/ebt'))
-  .use(require('ssb-db2/compat/log-stream'))
-  .use(require('ssb-db2/compat/history-stream'))
-  .use(require('ssb-deweird/producer'))
-  // Replication
-  .use(require('ssb-ebt')) // needs: db2/compat
-  .use(require('ssb-friends')) // needs: db2
-  .use(require('ssb-replication-scheduler')) // needs: friends, ebt
-  // Connections
-  .use(require('./plugins/multiserver-addons'))
-  .use(require('ssb-lan'))
-  .use(bluetoothTransport(keys, process.env.APP_DATA_DIR))
-  .use(require('ssb-conn')) // needs: db2, friends, lan, bluetooth
-  .use(require('ssb-conn-firewall')) // needs: friends
-  .use(require('ssb-room-client')) // needs: conn
-  .use(require('ssb-http-auth-client')) // needs: conn
-  .use(require('ssb-http-invite-client'))
-  .use(require('ssb-invite-client')) // needs: db2, conn
-  // Queries
-  .use(require('ssb-db2/about-self')) // needs: db2
-  .use(require('ssb-suggest-lite')) // needs: db2, about-self, friends
-  .use(require('ssb-threads')) // needs: db, db2, friends
-  .use(require('ssb-db2/full-mentions')) // needs: db2
-  .use(require('ssb-search2')) // needs: db2
-  // Blobs
-  .use(require('ssb-blobs'))
-  .use(require('ssb-serve-blobs')) // needs: blobs
-  .use(require('ssb-blobs-purge')) // needs: blobs, db2/full-mentions
-  // Customizations
-  .use(require('./plugins/aboutSelf')) // needs: db2
-  .use(require('./plugins/blobsUtils')) // needs: blobs
-  .use(require('./plugins/connUtils')) // needs: conn, aboutSelf
-  .use(require('./plugins/aliasUtils')) // needs: db2
-  .use(require('./plugins/resyncUtils')) // needs: db2, connFirewall
-  .use(require('./plugins/publishUtilsBack')) // needs: db, blobs, blobsUtils
-  .use(require('./plugins/searchUtils')) // needs: db2
-  .use(require('./plugins/keysUtils'))
-  .use(settingsUtils) // needs: blobs-purge
-  .use(require('./plugins/syncing')) // needs: db2
-  .use(require('./plugins/dbUtils')) // needs: db2, syncing
-  .use(require('./plugins/votes')) // needs: db2
-  .call(null, config);
