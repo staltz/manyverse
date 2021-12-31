bef46ec0a43be51695aa670c79a536788f4d1fe5
Andre Staltz <andre@staltz.com>
2021-12-31 14:14:35 +0200

    dx: [des] fixing missing dylib/dll for macOS/Win

--- desktop/electron-builder.config.js
+++ desktop/electron-builder.config.js
@@ -44,7 +44,9 @@ module.exports = {
   },
   files: [
     'node_modules/**/build/Release/*.node', // Node native modules
-    'node_modules/**/build/Release/*.so*', // Node native modules
+    'node_modules/**/build/Release/*.so*', // Node native modules (Linux)
+    'node_modules/**/build/Release/*.dylib', // Node native modules (macOS)
+    'node_modules/**/build/Release/*.dll', // Node native modules (Windows)
     'node_modules/sodium-chloride/index.js', // it bypasses noderify require()
     'node_modules/sodium-native/index.js', // needed for sodium-chloride
     'node_modules/node-gyp-build/index.js', // needed for sodium-chloride
