24fd26feae2f9a80247eec008053cc8a584ff807
Anders Rune Jensen <arj03@protonmail.ch>
2022-03-31 09:53:20 +0000

    ux: improve initial sync a bit, for new accounts
    Use startDecryptBox1 for new users

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -4,6 +4,8 @@
 
 import identity = require('./identity');
 const {restore, migrate} = identity;
+import ssb = require('./ssb');
+const {startSSB} = ssb;
 
 // Install Desktop backend plugins
 if (process.env.MANYVERSE_PLATFORM === 'desktop') {
@@ -56,17 +58,16 @@ if (process.env.MANYVERSE_PLATFORM === 'mobile') {
 
 // Setup initial communication with the frontend, to create or restore identity
 channel.addListener('identity', (request) => {
-  const startSSB = () => require('./ssb');
   let response: string;
   if (request === 'CREATE' || request === 'USE') {
-    startSSB();
+    startSSB(request === 'CREATE');
     response = 'IDENTITY_READY';
   } else if (request.startsWith('RESTORE:')) {
     const words = request.split('RESTORE: ')[1].trim();
     response = restore(words);
-    if (response === 'IDENTITY_READY') startSSB();
+    if (response === 'IDENTITY_READY') startSSB(false);
   } else if (request === 'MIGRATE') {
-    migrate(startSSB);
+    migrate(() => startSSB(false));
     response = 'IDENTITY_READY';
   } else {
     return;
