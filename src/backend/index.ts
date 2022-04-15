6e41707fc4a8f7e71efdeb3a4bfdc131503ac248
Andre Staltz <andre@staltz.com>
2022-04-15 16:27:43 +0300

    ux: bug fix: should not crash when recovering identity

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -56,20 +56,30 @@ if (process.env.MANYVERSE_PLATFORM === 'mobile') {
 }
 
 // Setup initial communication with the frontend, to create or restore identity
+let startedSSB = false;
 channel.addListener('identity', (request) => {
+  if (startedSSB) return;
   let response: string;
   if (request === 'CREATE') {
+    startedSSB = true;
     startSSB(true);
     response = 'IDENTITY_READY';
   } else if (request === 'USE') {
+    startedSSB = true;
     startSSB(false);
     response = 'IDENTITY_READY';
   } else if (request.startsWith('RESTORE:')) {
     const words = request.split('RESTORE: ')[1].trim();
     response = restore(words);
-    if (response === 'IDENTITY_READY') startSSB(false);
+    if (response === 'IDENTITY_READY') {
+      startedSSB = true;
+      startSSB(false);
+    }
   } else if (request === 'MIGRATE') {
-    migrate(() => startSSB(false));
+    migrate(() => {
+      startedSSB = true;
+      startSSB(false);
+    });
     response = 'IDENTITY_READY';
   } else {
     return;
