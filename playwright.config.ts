83ca9a9d63ed3900c840bcd11b567daa18ae0f68
Andre Staltz <andre@staltz.com>
2022-12-13 19:10:09 +0200

    dx: fix license check for playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2021-2022 The Manyverse Authors
+//
+// SPDX-License-Identifier: CC0-1.0
+
 import {PlaywrightTestConfig} from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
