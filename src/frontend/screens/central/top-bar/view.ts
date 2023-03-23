6235b2a40fb8f1d1cf74eaeaec4c12d580b090f0
Andre Staltz <andre@staltz.com>
2023-03-23 14:19:56 +0200

    ux: [des] bug fix: top bar title selectable

--- src/frontend/screens/central/top-bar/view.ts
+++ src/frontend/screens/central/top-bar/view.ts
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 The Manyverse Authors
+// SPDX-FileCopyrightText: 2022-2023 The Manyverse Authors
 //
 // SPDX-License-Identifier: MPL-2.0
 
@@ -104,6 +104,7 @@ export const styles = StyleSheet.create({
     ...Platform.select({
       web: {
         marginLeft: 0,
+        ['user-select']: 'none',
       },
       default: {
         marginLeft: Dimensions.horizontalSpaceLarge,
