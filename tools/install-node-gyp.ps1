ad4e494cadd369f14b49cb4a7b4d04cfda3cd77e
Jacob Karlsson <jacob.karlsson95@gmail.com>
2024-08-13 17:19:36 +0200

    Fix gyp version on windows

--- tools/install-node-gyp.ps1
+++ tools/install-node-gyp.ps1
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2021 The Manyverse Authors
+# SPDX-FileCopyrightText: 2021-2024 The Manyverse Authors
 #
 # SPDX-License-Identifier: Unlicense
 
@@ -7,4 +7,4 @@ npx node-gyp --verbose list
 npx node-gyp --verbose install $(node -v)
 npx node-gyp --verbose list
 # TODO resolve "node-gyp" cache path or set it for "node-gyp" explicitly rather than hardcoding the value
-ls "C:\Users\runneradmin\AppData\Local\node-gyp\Cache\$($(node -v).TrimStart('v'))\include\node"
+ls "C:\Users\runneradmin\AppData\Local\node-gyp\Cache\10.2.0\include\node"
